dc1908ebc838358b93317462e4adf6834eb58ae1
João Pedro <49282011+jpsoarxs@users.noreply.github.com>
2020-07-04 21:02:49 -0300

    added getStarted page

--- mobile/navigation.component.tsx
+++ mobile/navigation.component.tsx
@@ -4,12 +4,14 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import Tutorial from './src/pages/Tutorial';
 import Login from './src/pages/Login';
+import getStarted from './src/pages/GetStarted';
 
 const { Navigator, Screen } = createStackNavigator();
 
 export const AppNavigator = () => (
   <NavigationContainer>
     <Navigator headerMode='none'>
+      <Screen name='GetStarted' component={getStarted}/>
       <Screen name='Tutorial' component={Tutorial}/>
       <Screen name='Login' component={Login}/>
     </Navigator>
---
+++ mobile/src/pages/GetStarted/index.tsx
@@ -0,0 +1,92 @@
+import React,{useState} from 'react';
+import * as eva from '@eva-design/eva';
+import { Text, Button, Icon } from '@ui-kitten/components';
+import Swiper from 'react-native-swiper'
+import { LinearGradient } from 'expo-linear-gradient';
+import { SafeAreaView, View, Image, StyleSheet } from 'react-native'
+
+const getStarted: React.FC = ({ navigation }) => {
+
+  const [buttonHover, setButtonHover] = useState('#FFF');
+
+  const handlerPressJump = () => {
+    navigation.navigate("Login")
+  };
+
+  const hoverButton = () => {
+    setButtonHover("#D4D4D4")
+    setInterval(() => {setButtonHover("#FFF")},100)
+  }
+
+  const ArrowIcon = (props) => (
+    <Icon {...props} fill="#FC9272" name='arrow-forward'/>
+  );
+
+  return (
+    <LinearGradient
+    colors={['#FF5B7F', '#FC9272']}
+    style={{
+      left: 0,
+      right: 0,
+      top: 0,
+      height: 300,
+      flex:1
+    }}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View>
+          <Image style={styles.image} source={require('../../../assets/GetStartded/1.jpg')} />
+          <Text style={styles.title}>Letícia Oliveira</Text>
+          <Text style={styles.desc}>Sua conta está pronta! Clique em Comece agora para prosseguir.</Text>
+        </View>
+        <Button onPress={hoverButton} accessoryRight={ArrowIcon} style={[styles.button, {backgroundColor: buttonHover}]}>
+          <Text style={{color:"#FC9272"}}>Comece agora</Text>
+        </Button>
+      </SafeAreaView>
+    </LinearGradient>
+  );
+};
+
+const styles = StyleSheet.create({
+  image: {
+    alignSelf: "center",
+    marginTop: 216,
+    borderRadius: 130,
+    width: 130,
+    height: 130,
+    borderWidth: 4,
+    borderColor: "#FFF"
+  },
+  title: {
+    textAlign: "center",
+    marginTop: 64,
+    color: "#FFF",
+    fontSize: 18,
+    fontFamily: "Poppins_600SemiBold"
+  },
+  desc: {
+    textAlign: "center",
+    alignSelf: "center",
+    marginTop: 10,
+    color: "#FFF",
+    fontFamily: "Poppins_400Regular",
+    flexWrap: "wrap",
+    maxWidth: 400
+  },
+  button: {
+    alignSelf: "center",
+    marginTop: 64,
+    borderWidth: 0,
+    borderRadius: 23,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  }
+});
+
+export default getStarted;
